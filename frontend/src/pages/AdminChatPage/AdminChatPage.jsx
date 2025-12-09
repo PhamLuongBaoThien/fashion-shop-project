59865c20046d82f09eaa40782330ba6e74581ef7
Bao Thien <plbthien2004@gmail.com>
2025-12-09 21:32:46 +0700

    1.12.11

--- frontend/src/pages/AdminChatPage/AdminChatPage.jsx
+++ frontend/src/pages/AdminChatPage/AdminChatPage.jsx
@@ -138,7 +138,7 @@ const AdminChatPage = () => {
   useEffect(() => {
     if (user?.access_token) {
       const newSocket = io(ENDPOINT, {
-        transports: ["flashsocket", "websocket", "polling"],
+        transports: ["flashsocket"],
         withCredentials: true,
         reconnectionAttempts: 5,
         reconnectionDelay: 2000,
